d71517b84f79102061ae95ee3fe76ebd8f24e1ae
Amrithesh S <amritheshsiva@gmail.com>
2026-03-11 12:55:54 +0530

    admin side initial pages created

--- Admin/adminside/adminapp/views.py
+++ Admin/adminside/adminapp/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
-def Login(request):
-    return render(request,'Login.html')
-def Home(request):
-    return render(request,"Home.html")
+def login(request):
+    return render(request,'login.html')
+def home(request):
+    return render(request,"home.html")
+def doc(request):
+    return render(request,'doctors.html')
+def appointment(request):
+    return render(request,'appointments.html')
+def user(request):
+    return render(request,'user_manage.html')
+def userprofile(request):
+    return render(request,'userprofile.html')
+def report(request):
+    return render(request,'reports.html')
 
 
