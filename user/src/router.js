25cf1e014714903ad2633bb0de70747daea0167a
Amrithesh S <amritheshsiva@gmail.com>
2026-03-24 11:10:39 +0530

    routing

--- user/src/router.js
+++ user/src/router.js
@@ -2,13 +2,15 @@ import { createBrowserRouter } from "react-router-dom";
 import Login from "./components/login";
 import Register from "./components/register"
 import DocList from "./components/doclist"
+import Appmnt from "./components/appointments"
 import App from "./App";
 
 const router = createBrowserRouter([
     { path: '', element: <App/> },
     { path: 'login', element: <Login/> },
     {path:'register',element:<Register/>},
-    {path:'doclist',element:<DocList/>}
+    {path:'doclist',element:<DocList/>},
+    {path:'appointments',element:<Appmnt/>}
 ]);
 
 export default router;
