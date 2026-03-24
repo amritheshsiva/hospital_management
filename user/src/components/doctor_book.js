c355fc2718f83baa947d30819cab6c8169248a4d
Amrithesh S <amritheshsiva@gmail.com>
2026-03-24 11:22:47 +0530

    booking page

---
+++ user/src/components/doctor_book.js
@@ -0,0 +1,44 @@
+import React from "react";
+import Navbar from "../components/navbar";
+
+function Appointment() {
+  return (
+    <div>
+      <Navbar />
+
+      <div className="container mt-4">
+        <h3 className="text-center mb-4 fw-bold">
+          Book Appointment
+        </h3>
+        <div className="card p-3 mb-4 shadow-sm border-0">
+          <div className="d-flex align-items-center">
+            <img src="https://imgs.search.brave.com/HL1iM8eX1ZJ0a7abH7FCDElbCbpXmY3-2mQbxAwzYMY/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly90aHVt/YnMuZHJlYW1zdGlt/ZS5jb20vYi9mYWNl/bGVzcy1tYWxlLWRv/Y3Rvci1hdmF0YXIt/c3RldGhvc2NvcGUt/ZmxhdC1kZXNpZ24t/aWRlYWwtaGVhbHRo/Y2FyZS1tZWRpY2Fs/LWFwcHMtYW5vbnlt/b3VzLXByb2Zlc3Np/b25hbC1wcm9maWxl/cy0zODgzMzMyODIu/anBn" alt="doc"className='rounded-circle me-3 mr-2' style={{width:'80px', height:'80px', objectFit:'cover'}}/>
+            <div>
+              <h5 className="mb-1">Dr. Arjun</h5>
+              <p className="text-muted mb-1">MBBS, MD (Cardiology)</p>
+              <span className="badge bg-light text-danger">Cardiology</span>
+            </div>
+           </div>
+        </div>
+
+
+        <div className="card p-4 shadow-sm border-0">
+          <h6>Select Appointment Date</h6>
+          <input type="date" className="form-control mb-4" />
+          <h6>Select Time Slot</h6>
+          <div className="d-flex flex-wrap gap-3 mb-4">
+            <button className="btn btn-outline-primary mr-2">9:00 AM</button>
+            <button className="btn btn-outline-primary mr-2">10:00 AM</button>
+            <button className="btn btn-outline-primary mr-2">11:00 AM</button>
+            <button className="btn btn-outline-primary mr-2">4:00 PM</button>
+            <button className="btn btn-outline-primary mr-2">5:00 PM</button>
+            <button className="btn btn-outline-primary mr-2">6:00 PM</button>
+          </div>
+          <button className="btn btn-primary w-100">Confirm Appointment</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Appointment;
