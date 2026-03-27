d49405c1e09a20c3889d1715b76363d79ed5a3f9
Amrithesh S <amritheshsiva@gmail.com>
2026-03-27 11:03:01 +0530

    table creation

--- Admin/adminside/adminside/settings.py
+++ Admin/adminside/adminside/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'adminside.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'hospital)management',
+        'NAME': 'hospital_management',
         'USER': 'root',
         'PASSWORD': '',
         'HOST': 'localhost',
@@ -125,3 +125,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'adminapp.User'
